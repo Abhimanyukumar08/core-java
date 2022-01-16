c2108de534263f44b88d290de14c863c29b1f8cc
Abhimanyu Kumar <abhimanyuthebes1@gmail.com>
2022-01-16 15:28:31 +0530

    update

---
+++ Practice/Demo.java
@@ -0,0 +1,19 @@
+class Demo { 
+    public static void main(String[] args) {
+        String result: int which = 2: 
+        switch (which) { 
+            case 0: result = "0": break; 
+            case 1: result = "1"; break; 
+            case 2: result = "2"; 
+            default: result = "default";} 
+            System.out.println("Result: " + result); 
+        } }
+//     public static void main(String[] ar) {
+//         String strl = new String("TECHNOLOGY"); 
+//         String str2 = new String("TECHNOLOGY"); 
+//         System.out.println(strl = str2); 
+//         System.out.println(strl.equals(str2)); 
+//         int a[]=new int[]{10,20,30,40};
+//         System.out.println(a);
+//     } 
+// }
